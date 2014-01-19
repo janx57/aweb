93a737031cc91d8594dbd5ab13e6f380bfc97b8d
janx57 <janek.surf@gmail.com>
2014-01-19 18:40:16 +0100

    Implement HTTP HEAD test

--- aweb-acceptance-tests/src/test/java/com/janx57/aweb/tests/GetIT.java
+++ aweb-acceptance-tests/src/test/java/com/janx57/aweb/tests/GetIT.java
@@ -11,6 +11,7 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,13 +19,13 @@ import org.junit.Test;
 public class GetIT extends AbstractServerTest {
 
   @Test
-  public void getTest() throws ClientProtocolException, IOException, InterruptedException {
-    latch.await();
-
+  public void getTest() throws ClientProtocolException, IOException,
+      InterruptedException {
     String url = String.format("http://%s:%s/index.html", host, port);
-
     HttpClient client = HttpClientBuilder.create().build();
     HttpGet request = new HttpGet(url);
+
+    latch.await();
 
     HttpResponse response = client.execute(request);
 
@@ -49,5 +50,30 @@ public class GetIT extends AbstractServerTest {
     Assert.assertFalse(targetFile.hasNextLine());
 
     targetFile.close();
+  }
+
+  @Test
+  public void headTest() throws InterruptedException, ClientProtocolException, IOException {
+    String url = String.format("http://%s:%s/index.html", host, port);
+    HttpHead request = new HttpHead(url);
+    HttpClient client = HttpClientBuilder.create().build();
+
+    latch.await();
+
+    HttpResponse response = client.execute(request);
+
+    StatusLine sl = response.getStatusLine();
+    Assert.assertEquals(200, sl.getStatusCode());
+    Assert.assertEquals("OK", sl.getReasonPhrase());
+
+    ProtocolVersion pv = response.getProtocolVersion();
+    Assert.assertEquals("HTTP", pv.getProtocol());
+    Assert.assertEquals(1, pv.getMajor());
+    Assert.assertEquals(1, pv.getMinor());
+
+    int contentLength = Integer.parseInt(response.getLastHeader("Content-Length").getValue());
+    Assert.assertEquals(68, contentLength);
+
+    Assert.assertNull(response.getEntity());
   }
 }
